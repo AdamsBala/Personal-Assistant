8cc1877e50f4d501b6a54a4f238531ffc912b478
Adams Bala <adams.alok@gmail.com>
2023-12-09 18:44:02 -0500

    Code refactor

--- landing.py
+++ landing.py
@@ -5,18 +5,17 @@ from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from flask import Blueprint, render_template, request, jsonify
 
-views = Blueprint(__name__, "views")
-
+landing = Blueprint(__name__, "landing")
 loader = DirectoryLoader("./data", glob="*.txt")
 index = VectorstoreIndexCreator().from_loaders([loader])
 
 
-@views.route("/")
+@landing.route("/")
 def home():
     return render_template("index.html")
 
 
-@views.route("/query", methods=['POST'])
+@landing.route("/query", methods=['POST'])
 def get_data():
     query = request.json['messages'][-1]['content']
     data = index.query(query)
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from dotenv import load_dotenv
 import os
 from flask import Flask
-from views import views
+from landing import landing
 
 load_dotenv()
-
-
 os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
 
-
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/views")
+app.register_blueprint(landing, url_prefix="/landing")
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
